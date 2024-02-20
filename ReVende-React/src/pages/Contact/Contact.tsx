73e1e48eb0b2914a6da74f880a3868aee816eef4
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-20 15:40:57 -0300

    Update Contact.tsx

--- ReVende-React/src/pages/Contact/Contact.tsx
+++ ReVende-React/src/pages/Contact/Contact.tsx
@@ -6,26 +6,62 @@ const Contact = () => {
 
         <h3 className="text-4xl text-white">Entre em contato com os desenvolvedores do website:</h3>
 
-        <div className="grid justify-items-start text-2xl text-white">
+        <div className="justify-items-start text-2xl text-white">
           <table>
             <th>
               <tr>
                 Eloísa
               </tr>
               <tr>
+
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
                 Fernando Lopes
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
+                
               </tr>
               <tr>
                 Jaqueline Paula
               </tr>
               <tr>
+                
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
                 Leo Duarte
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
+                
               </tr>
               <tr>
                 Marina Pereira
               </tr>
               <tr>
+                
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
                 Marília Tostes
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
+                
               </tr>
               <tr>
                 Rafa

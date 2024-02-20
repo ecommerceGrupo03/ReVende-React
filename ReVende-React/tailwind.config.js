c8f77955cce1742c5e0f425226046ec4aa715d93
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-20 13:59:57 -0300

    Update tailwind.config.js

--- ReVende-React/tailwind.config.js
+++ ReVende-React/tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [ "./index.html",
-  "./src/*/.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,ts,jsx,tsx}",
   "*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-

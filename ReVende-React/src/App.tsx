f250ff32727078ecab97a817e61f37a2c0a847de
marina-mmap <marinapereira.mmap@gmail.com>
2024-02-20 16:17:07 -0300

    NavBar com Rotas

--- ReVende-React/src/App.tsx
+++ ReVende-React/src/App.tsx
@@ -1,17 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import NavBar from './components/navBar/NavBar'
 import Home from './pages/Home/Home'
+import Contact from './pages/Contact/Contact'
+import About from './pages/About/About'
 
 function App() {
 
   return (
     <>
-    <div className='bg-lime-600 size-auto'>
-      <NavBar/>
-      <Home/>
-      <Footer/>
-      </div>
+      <BrowserRouter>
+        <NavBar />
+        <div className='bg-lime-600 size-auto'>
+        <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/home" element={<Home />} />
+        </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- ReVende-React/src/components/navBar/NavBar.tsx
+++ ReVende-React/src/components/navBar/NavBar.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom"
+import About from "../../pages/About/About"
+
 function NavBar() {
  
     return (
@@ -7,8 +10,10 @@ function NavBar() {
               <div className='text-2xl font-bold uppercase'>ReVende</div>
   
               <div className='flex gap-4'>
-                <div className='hover:underline'>Home</div>
-                <div className='hover:underline'>Produtos</div>
+                <Link to='/home' className='hover:underline'>Home</Link>
+                <Link to='/contact' className='hover:underline'>Contact</Link>
+                <Link to='/about' className='hover:underline'>About</Link>
+                <div className='hover:underline'>Produtos</div> 
                 <div className='hover:underline'>Categorias</div>
                 <div className='hover:underline'>Perfil</div>
                 <div className='hover:underline'>Sair</div>

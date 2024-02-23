eaf7af4a0914b8548c8ceb8a28e9fe986ce56e96
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-23 15:21:31 -0300

    Upload das informações para contato
    Cards atualizados

--- ReVende-React/src/components/contactCard/ContactCard.tsx
+++ ReVende-React/src/components/contactCard/ContactCard.tsx
@@ -11,7 +11,7 @@ export interface ContactProps{
 
 function ContactCard({nome, sobre, imagem, linkedin, github}:ContactProps) {
   return (
-    <div className="flex justify-center grid-cols-3 bg-gradient-to-r from-gray-900 to-gray-700 border-2 border-gray-700 rounded-lg items-stretch">
+    <div className="flex justify-center grid-cols-3 bg-gradient-to-r from-gray-900 to-gray-700 border-2 border-gray-700 rounded-lg items-stretch my-10">
         <div className="flex items-center m-12">
             <img src={imagem} alt={nome} className="rounded-full size-64"/>
         </div>
--- ReVende-React/src/pages/Contact/Contact.tsx
+++ ReVende-React/src/pages/Contact/Contact.tsx
@@ -4,17 +4,65 @@ const Contact = () => {
   return (
     <>
       <div className="my-10 flex content-center bg-lime-600 flex-col items-center justify-center">
-        <h1 className="text-8xl text-white">Contato</h1>
+        <h1 className="text-8xl text-white mt-20">Contato</h1>
 
-        <h3 className="text-4xl text-white">Entre em contato com os desenvolvedores do website:</h3>
+        <h3 className="text-4xl text-white mt-10 mb-40">Entre em contato com os desenvolvedores do website:</h3>
 
-        <div className="justify-items-start text-2xl text-white my-12">
+        <div className="justify-items-start text-2xl text-white my-10">
+          <ContactCard
+            nome="Eloísa Marin"
+            sobre="Desenvolvedora Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/74751076?s=60&v=4"
+            linkedin="https://www.linkedin.com/in/eloisamarin/"
+            github="https://github.com/eloisamarin"
+          />
+
           <ContactCard
             nome="Fernando Lopes"
             sobre="Desenvolvedor Java Full-Stack Júnior"
             imagem="https://avatars.githubusercontent.com/u/118869201?v=4"
             linkedin="https://www.linkedin.com/in/fernando-barbosa-ferreira-lopes/"
             github="https://github.com/plasmus777"
+          />
+
+          <ContactCard
+            nome="Jaqueline Paula"
+            sobre="Desenvolvedora Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/87137086?v=4"
+            linkedin="https://www.linkedin.com/in/jaquelinepaula/"
+            github="https://github.com/JaquePaula"
+          />
+
+          <ContactCard
+            nome="Leonardo Duarte"
+            sobre="Desenvolvedor Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/133834707?v=4"
+            linkedin="https://www.linkedin.com/in/leonardo-rodrigues-de-sousa-duarte-714bba260/"
+            github="https://github.com/LeoDuarte37"
+          />
+
+          <ContactCard
+            nome="Marina Pereira"
+            sobre="Desenvolvedora Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/146123948?s=60&v=4"
+            linkedin="https://www.linkedin.com/in/marina-alexandre-pereira/"
+            github="https://github.com/marina-mmap"
+          />
+
+          <ContactCard
+            nome="Marília Tostes"
+            sobre="Desenvolvedora Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/133540781?v=4"
+            linkedin="https://www.linkedin.com/in/marilia-ribeiro-tostes/"
+            github="https://github.com/mariliatostesdev"
+          />
+
+          <ContactCard
+            nome="Rafael Carmo"
+            sobre="Desenvolvedor Java Full-Stack Júnior"
+            imagem="https://avatars.githubusercontent.com/u/151798963?s=60&v=4"
+            linkedin="https://www.linkedin.com"
+            github="https://github.com/Rflpmt"
           />
         </div>
       </div>

c7b2e3ba367df016eb7d277a2ec1de49572d4a9b
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-23 16:25:52 -0300

    Renomear Arquivos (consistência) e link github

--- ReVende-React/src/App.tsx
+++ ReVende-React/src/App.tsx
@@ -4,9 +4,9 @@ import Footer from './components/footer/Footer'
 import NavBar from './components/navBar/NavBar'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
-import Home from './pages/Home/Home'
-import Contact from './pages/Contact/Contact'
-import About from './pages/About/About'
+import Home from './pages/home/Home'
+import Contact from './pages/contact/Contact'
+import About from './pages/about/About'
 import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
--- ReVende-React/src/components/footer/Footer.tsx
+++ ReVende-React/src/components/footer/Footer.tsx
@@ -1,18 +1,15 @@
-import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react"
+import { GithubLogo } from "@phosphor-icons/react"
+import { Link } from "react-router-dom"
 function Footer() {
-
-
 
     return ( 
         <>
             <div className="flex justify-center bg-[#308C50] text-white">
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>ReVende | Copyright: </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-xl font-bold'>ReVende | 2024</p>
+                    <p className='text-lg'>Acesse os arquivos do projeto:</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <GithubLogo size={48} weight='bold' />
-                        
+                        <Link to="https://github.com/ecommerceGrupo03"><GithubLogo size={48} weight='bold'/></Link>
                     </div>
                 </div>
             </div>
@@ -21,3 +18,6 @@ function Footer() {
 }
 
 export default Footer
+
+//import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react"
+//<LinkedinLogo size={48} weight='bold' />

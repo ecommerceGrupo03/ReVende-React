a72d467d47ec2db66723cee4845f8a3dc88e59b1
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-02-23 16:16:06 -0300

    Atualização da página Sobre

--- ReVende-React/src/pages/About/About.tsx
+++ ReVende-React/src/pages/About/About.tsx
@@ -2,9 +2,11 @@ const About = () => {
   return (
     <>
       <div className="my-10 flex content-center bg-lime-600 flex-col items-center justify-center">
-        <h1 className="text-8xl text-white">Sobre</h1>
+        <h1 className="text-8xl text-white mt-20">Sobre o Projeto</h1>
 
-        <h3 className="text-4xl text-white">O projeto ReVende tem como objetivo </h3>
+        <p className="text-4xl text-white mt-16 mx-16">Este projeto é um E-commerce de produtos usados ou sem uso, tendo como inspiração o Objetivo de Desenvolvimento Sustentável (ODS) número 12 da ONU- Consumo e Produção Sustentáveis.</p>
+
+        <p className="text-4xl text-white mt-10 mb-40 mx-16">Nosso objetivo é criar uma plataforma que facilite a conexão entre pessoas que desejam descartar ou vender materiais usados ou sem uso e pessoas interessadas em comprá-los.</p>
 
         <div className="grid justify-items-start text-2xl text-white">
           

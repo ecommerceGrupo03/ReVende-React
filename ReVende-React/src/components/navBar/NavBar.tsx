fb43126d89a30fed3f443f57dc997ba9cece68e0
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-29 10:06:57 -0300

    NavBar color change + logo

--- ReVende-React/src/components/navBar/NavBar.tsx
+++ ReVende-React/src/components/navBar/NavBar.tsx
@@ -1,41 +1,51 @@
-import { useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
-import logoRevende from "../../assets/logoRevende.svg"
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
+import logoRevende from '../../assets/logoRevende.svg';
 
 function NavBar() {
-  let navigate = useNavigate()
+	let navigate = useNavigate();
 
-  const { usuario, handleLogout } = useContext(AuthContext)
+	const { usuario, handleLogout } = useContext(AuthContext);
 
-  function logout() {
-      handleLogout()
-      alert('Usuário deslogado com sucesso.')
-      navigate('/login')
-  }
+	function logout() {
+		handleLogout();
+		alert('Usuário deslogado com sucesso.');
+		navigate('/login');
+	}
 
-    return (
-      <>
-       <div className='w-full bg-[#308C50] text-white flex justify-center'>
-            <div className="container flex justify-between text-lg">
-              <div className='text-2xl font-bold uppercase shrink-0 border-2'>
-                <img src={logoRevende} alt="ReVende" className="w-37 h-16"/>
-              </div>
-  
-              <div className='flex gap-4 text-xl py-4'>
-                <Link to='/home' className='hover:underline'>Início</Link>
-                <Link to='/contact' className='hover:underline'>Contato</Link>
-                <Link to='/about' className='hover:underline'>Sobre</Link>
-                <Link to='/login' className='hover:underline'>Login</Link>
-                <div className='hover:underline'>Produtos</div> 
-                <div className='hover:underline'>Categorias</div>
-                <div className='hover:underline'>Perfil</div>
-                <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-              </div>
-            </div>
-          </div>
-      </>
-    )
-  }
-  
-  export default NavBar
+	return (
+		<>
+			<div className="w-full bg-[#FFF] text-slate-900 flex py-4 justify-center">
+				<div className="w-full container flex justify-between text-lg items-center">
+					<div className="text-2xl font-bold uppercase text-[#1C3240]">
+						<img src={logoRevende} alt="Logo Revende" className="w-1/2" />
+					</div>
+
+					<div className="flex gap-4">
+						<Link to="/home" className="hover:underline">
+							Início
+						</Link>
+						<Link to="/contact" className="hover:underline">
+							Contato
+						</Link>
+						<Link to="/about" className="hover:underline">
+							Sobre
+						</Link>
+						<Link to="/login" className="hover:underline">
+							Login
+						</Link>
+						<div className="hover:underline cursor-pointer">Produtos</div>
+						<div className="hover:underline cursor-pointer">Categorias</div>
+						<div className="hover:underline cursor-pointer">Perfil</div>
+						<Link to="" onClick={logout} className="hover:underline">
+							Sair
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default NavBar;
--- ReVende-React/src/pages/Home/Home.tsx
+++ ReVende-React/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
 	return (
 		<>
-			<div className="my-10 flex content-center bg-lime-600 flex-col items-center justify-center">
+			<div className="mb-10 mt-0 flex content-center bg-lime-600 flex-col items-center justify-center">
 				<h1 className="text-8xl text-white">Home</h1>
 
 				<h3 className="text-4xl text-white">Bem Vindo ao ReVende!</h3>

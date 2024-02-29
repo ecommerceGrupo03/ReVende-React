1181c72064ab86dc41378f8c8b938f6d3cb3f9fa
mariliatostesdev <mahtostes.dev@gmail.com>
2024-02-29 09:59:27 -0300

    update folder names

--- ReVende-React/src/pages/Home/Home.tsx
+++ ReVende-React/src/pages/Home/Home.tsx
@@ -1,15 +1,12 @@
-
 const Home = () => {
-  return (
-    <>
-    <div className="my-10 flex content-center bg-lime-600 flex-col items-center justify-center">
-      <h1 className="text-8xl text-white">Home</h1>
+	return (
+		<>
+			<div className="my-10 flex content-center bg-lime-600 flex-col items-center justify-center">
+				<h1 className="text-8xl text-white">Home</h1>
 
-      <h3 className="text-4xl text-white">Bem Vindo ao ReVende!</h3>
-      </div>
-      </>
-
-  )
-}
-
-export default Home
+				<h3 className="text-4xl text-white">Bem Vindo ao ReVende!</h3>
+			</div>
+		</>
+	);
+};
+export default Home;

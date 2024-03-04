d1a0fad4793043fdfe2b220c3ed41caed90c48e1
Eloisa Marin <eloisamarin7@gmail.com>
2024-03-04 16:12:24 -0300

    Add Card deletar Categoria

--- ReVende-React/src/components/categoria/cardCategoria/CardCategoria.tsx
+++ ReVende-React/src/components/categoria/cardCategoria/CardCategoria.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import Categoria from '../../../models/Categoria'
+import { Image } from '@phosphor-icons/react'
 
 interface CardCategoriaProps {
   categoria: Categoria
@@ -7,16 +8,23 @@ interface CardCategoriaProps {
 
 function CardCategorias({categoria}: CardCategoriaProps) {
   return (
-    <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-      <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2xl'>{categoria.nome}</header>
-      <p className='p-8 text-3xl bg-slate-200 h-full'>{categoria.descricao}</p>
-      <div className="flex">
-        <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2'>
+    
+    <div className='border shadow-xl flex flex-col rounded-2xl overflow-hidden justify-between'>
+      
+      <div className="justify-center  flex align-center  bg-white box-shadow: 0 1px 2px 0 ">
+      <Image size={100} color="#8e3333 " />
+      </div>
+      <header className='pt-6 pl-6  text-sm text-gray-600 bg-white  '>{categoria.nome}</header>
+      <p className='pl-6 pb-6 text-base font-semibold text-gray-900 bg-white'>{categoria.descricao}</p>
+      
+      <div className="flex border shadow-xl">
+        <Link to={`/editarCategoria/${categoria.id}`} className='w-full text-slate-100 bg-blue-500 hover:bg-blue-600 flex items-center justify-center py-2'>
           <button>Editar</button>
         </Link>
-        <Link to={`/deletarCategoria/${categoria.id}`} className='text-slate-100 bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+        <Link to={`/deletarCategoria/${categoria.id}`} className='text-slate-100 bg-red-600 hover:bg-red-700 w-full flex items-center justify-center'>
           <button>Deletar</button>
         </Link>
+
       </div>
     </div>
   )

654aa5cf6f131026c2010a0942dddeb2d43c86aa
mariliatostesdev <mahtostes.dev@gmail.com>
2024-03-04 17:36:40 -0300

    Iniciado Profile e Home

--- ReVende-React/src/components/navBar/NavBar.tsx
+++ ReVende-React/src/components/navBar/NavBar.tsx
@@ -1,4 +1,3 @@
-
 import { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
@@ -9,12 +8,13 @@ import Carrinho from '../carrinho/Carrinho';
 import { CarrinhoContext } from '../../contexts/CarrinhoContext';
 
 function NavBar() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const { usuario, handleLogout } = useContext(AuthContext);
+	const { usuario, handleLogout } = useContext(AuthContext);
 
-  const [carrinhoOpen, setCarrinhoOpen] = useState(false);
+	const [carrinhoOpen, setCarrinhoOpen] = useState(false);
 
+<<<<<<< Updated upstream
   const ctx = useContext(CarrinhoContext);
 
   function logout() {
@@ -34,10 +34,91 @@ function NavBar() {
   return (
     <>
       {carrinhoOpen == true && <Carrinho />}
+=======
+	function logout() {
+		handleLogout();
+		toastAlerta('Usuário deslogado com sucesso.', 'sucesso');
+		navigate('/login');
+	}
 
-      <div className="flex flex-wrap place-items-center">
-        <section className="relative mx-auto">
+	function handleCarrinhoOpen() {
+		if (carrinhoOpen) {
+			setCarrinhoOpen(false);
+		} else {
+			setCarrinhoOpen(true);
+		}
+	}
 
+	return (
+		<>
+			{carrinhoOpen == true && <Carrinho />}
+>>>>>>> Stashed changes
+
+			<div className="flex flex-wrap place-items-center">
+				<section className="relative mx-auto">
+					{/* <!-- navbar --> */}
+					<nav className="flex justify-between bg-gradient-to-b from-[#85A693] to-[#f2f2f2] text-[#1C3240] w-screen">
+						<div className="px-5 xl:px-12 py-6 flex w-full items-center">
+							<a className="text-3xl font-bold font-heading" href="#">
+								<img className="h-16" src={logoRevende} alt="logo" />
+							</a>
+							{/* <!-- Nav Links --> */}
+							<ul className="hidden md:flex px-4 mx-auto font-semibold text-lg space-x-12">
+								<li>
+									<Link
+										to="/home"
+										className=" hover:text-black  hover:underline"
+									>
+										Home
+									</Link>
+								</li>
+								<li>
+									<Link
+										to="/about"
+										className="hover:text-black  hover:underline"
+									>
+										Sobre
+									</Link>
+								</li>
+								<li>
+									<Link
+										to="/produtos"
+										className="hover:text-black  hover:underline"
+									>
+										Produtos
+									</Link>
+								</li>
+								<li>
+									<Link
+										to="/categorias"
+										className="hover:text-black hover:underline"
+									>
+										Categorias
+									</Link>
+								</li>
+							</ul>
+							{/* <!-- Header Icons --> */}
+							<div className="hidden xl:flex items-center space-x-5">
+								<Link
+									to=""
+									className="flex items-center hover:text-gray-600"
+									onClick={handleCarrinhoOpen}
+								>
+									<ShoppingCart size={26} color="#1C3240" weight="bold" />
+									<span className="flex absolute -mt-5 ml-4">
+										<span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+										<span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
+									</span>
+								</Link>
+								{/* <!-- Perfil     --> */}
+								<Link
+									to="/perfil"
+									className="flex items-center hover:text-gray-600"
+								>
+									<UserCircle size={26} color="#1C3240" weight="bold" />
+								</Link>
+
+<<<<<<< Updated upstream
           {/* <!-- navbar --> */}
           <nav className="flex justify-between bg-gradient-to-b from-[#85A693] to-[#f2f2f2] text-[#1C3240] w-screen">
             <div className="px-5 xl:px-12 py-6 flex w-full items-center">
@@ -108,6 +189,55 @@ function NavBar() {
 
     </>
   );
+=======
+								<Link to="" onClick={logout} className="hover:text-gray-600">
+									<SignOut size={26} color="#1C3240" weight="bold" />
+								</Link>
+							</div>
+						</div>
+						{/* <!-- Responsive navbar --> */}
+						<a className="xl:hidden flex mr-6 items-center" href="#">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6 hover:text-gray-200"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+								/>
+							</svg>
+							<span className="flex absolute -mt-5 ml-4">
+								<span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+								<span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
+							</span>
+						</a>
+						<a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6 hover:text-gray-200"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M4 6h16M4 12h16M4 18h16"
+								/>
+							</svg>
+						</a>
+					</nav>
+				</section>
+			</div>
+		</>
+	);
+>>>>>>> Stashed changes
 }
 
 export default NavBar;
--- ReVende-React/src/pages/perfil/Perfil.tsx
+++ ReVende-React/src/pages/perfil/Perfil.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
-import perfilCapa from '../../assets/perfil-capa.jpg';
 import { toastAlerta } from '../../util/toastAlerta';
+import { Pencil } from '@phosphor-icons/react';
 function Perfil() {
 	let navigate = useNavigate();
 
@@ -21,24 +21,121 @@ function Perfil() {
 	}, [usuario.token]);
 
 	return (
-		<div className="container mx-auto pb-10 rounded-2xl overflow-hidden">
-			<img
-				className="w-full h-72 object-cover border-b-8 pt-10 border-white"
-				src={perfilCapa}
-				alt="Capa do Perfil"
+		// <div className="container mx-auto pb-10 rounded-2xl overflow-hidden">
+		// 	<img
+		// 		className="w-full h-72 object-cover border-b-8 pt-10 border-white"
+		// 		src={perfilCapa}
+		// 		alt="Capa do Perfil"
+		// 	/>
+		// 	<img
+		// 		src={usuario.foto}
+		// 		alt={`Foto de perfil de ${usuario.nome}`}
+		// 		className="rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10"
+		// 	/>
+		// 	<div className="relative mt-[-9rem] h-72 flex flex-col bg-[#308C50] text-white text-2xl items-center text-center justify-center">
+		// 		<div className="pt-28">
+		// 			<p>Nome: {usuario.nome} </p>
+		// 			<p>Email: {usuario.email}</p>
+		// 		</div>
+		// 	</div>
+		// </div>
+		<>
+			<link
+				rel="stylesheet"
+				href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
 			/>
-			<img
-				src={usuario.foto}
-				alt={`Foto de perfil de ${usuario.nome}`}
-				className="rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10"
+			<link
+				rel="stylesheet"
+				href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
 			/>
-			<div className="relative mt-[-9rem] h-72 flex flex-col bg-[#308C50] text-white text-2xl items-center text-center justify-center">
-				<div className="pt-28">
-					<p>Nome: {usuario.nome} </p>
-					<p>Email: {usuario.email}</p>
-				</div>
-			</div>
-		</div>
+			<main className=" pt-10 h-[75vh] profile-page bg-gradient-to-t from-[#85A693] to-[#f2f2f2] text-[#1C3240]">
+				<section className="relative block h-80">
+					<div className="absolute top-0 w-full h-full bg-center bg-cover"></div>
+					<div
+						className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+						style={{ transform: 'translateZ(0px)' }}
+					>
+						<svg
+							className="absolute bottom-0 overflow-hidden"
+							xmlns="http://www.w3.org/2000/svg"
+							preserveAspectRatio="none"
+							version="1.1"
+							viewBox="0 0 2560 100"
+							x={0}
+							y={0}
+						></svg>
+					</div>
+				</section>
+				<section className="relative pt-16 pb-16">
+					<div className="container mx-auto px-4">
+						<div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+							<div className="px-6">
+								<div className="flex flex-wrap justify-center">
+									<div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+										<div className="relative">
+											<img
+												alt="..."
+												src={usuario.foto}
+												className="shadow-xl rounded-full object-cover align-middle border-none -m-16 -ml-20 lg:-ml-16 min-w-36 h-36 shrink-0 grow-0"
+											/>
+										</div>
+									</div>
+									<div className="w-full lg:w-4/12 lg:order-3 lg:text-right lg:self-center flex justify-end">
+										<div className="py-6 px-16 mt-32 sm:mt-0">
+											<button
+												className="bg-blue-500 hover:bg-blue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none flex gap-2 focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+												type="button"
+											>
+												<Pencil size={15} />
+												Editar
+											</button>
+										</div>
+									</div>
+									<div className="w-full lg:w-4/12 px-4 lg:order-1">
+										<div className="flex justify-center py-4 lg:pt-4">
+											<div className="mr-4 p-3 text-center">
+												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+													22
+												</span>
+												<span className="text-sm text-blueGray-400">
+													Compras
+												</span>
+											</div>
+											<div className="mr-4 p-3 text-center">
+												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+													10
+												</span>
+												<span className="text-sm text-blueGray-400">
+													Produtos
+												</span>
+											</div>
+											<div className="lg:mr-4 p-3 text-center">
+												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+													4
+												</span>
+												<span className="text-sm text-blueGray-400">
+													Avaliações
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="text-center mt-8">
+									<h3 className="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">
+										{usuario.nome}
+									</h3>
+									<div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+										<i className="mr-2 text-lg text-blueGray-400" />
+										{usuario.email}
+									</div>
+								</div>
+								<hr className="my-16 w-1/2 mx-auto" />
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+		</>
 	);
 }
 

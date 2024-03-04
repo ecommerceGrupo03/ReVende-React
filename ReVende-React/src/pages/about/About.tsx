f7338931776ffc1836c7b1bc7f7b189d04aa4cbf
JaquePaula <jaquemarques.santos@gmail.com>
2024-03-04 20:22:03 -0300

    fix: position image

--- ReVende-React/src/pages/about/About.tsx
+++ ReVende-React/src/pages/about/About.tsx
@@ -1,5 +1,5 @@
-import encomendaFoto from '../../assets/encomenda.jpg';
-import odsObjetivo12 from '../../assets/12ods.png';
+import encomendaFoto from "../../assets/encomenda.jpg";
+import odsObjetivo12 from "../../assets/12ods.png";
 
 const About = () => {
   return (
@@ -14,22 +14,27 @@ const About = () => {
                 sustentando o futuro
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
-              Nossa missão é contribuir para o desenvolvimento sustentável global, alinhando nossas operações com o Objetivo 12 da ODS - ONU. 
-              Através do nosso e-commerce, buscamos promover práticas comerciais responsáveis e conscientizar nossos clientes sobre a importância da sustentabilidade em suas escolhas de compra, troca e reutilização de insumos.
-              Ao escolher ReVende, você está apoiando não apenas uma experiência de compra conveniente, mas também um futuro mais sustentável para o nosso planeta e para as gerações futuras. 
+                Nossa missão é contribuir para o desenvolvimento sustentável
+                global, alinhando nossas operações com o Objetivo 12 da ODS -
+                ONU. Através do nosso e-commerce, buscamos promover práticas
+                comerciais responsáveis e conscientizar nossos clientes sobre a
+                importância da sustentabilidade em suas escolhas de compra,
+                troca e reutilização de insumos. Ao escolher ReVende, você está
+                apoiando não apenas uma experiência de compra conveniente, mas
+                também um futuro mais sustentável para o nosso planeta e para as
+                gerações futuras.
               </p>
             </div>
           </div>
         </div>
-        <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+        <div className="marker: inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute lg:top-12 lg:right-6 xl:px-0">
           <img
-            className="object-cover w-full h-48 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+            className="object-cover rounded-xl  lg:w-full lg:h-full shadow-lg sm:h-96"
             src={encomendaFoto}
             alt=""
           />
         </div>
       </div>
-
 
       <div className="relative flex flex-col pt-16 lg:pt-0 lg:flex-col lg:pb-0">
         <div className="flex flex-col items-end w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
@@ -40,9 +45,12 @@ const About = () => {
                 {/* Consumo e Produção Sustentáveis */}
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
-                Aborda a temática da produção e consumo sustentáveis, concentrando-se em iniciativas tanto globais quanto locais. 
-                Isso envolve a promoção do uso eficiente dos recursos naturais, a minimização do desperdício de alimentos e a gestão responsável de resíduos químicos. 
-                Este objetivo também engloba o cuidado com os resíduos sólidos e a redução da emissão de poluentes. 
+                Aborda a temática da produção e consumo sustentáveis,
+                concentrando-se em iniciativas tanto globais quanto locais. Isso
+                envolve a promoção do uso eficiente dos recursos naturais, a
+                minimização do desperdício de alimentos e a gestão responsável
+                de resíduos químicos. Este objetivo também engloba o cuidado com
+                os resíduos sólidos e a redução da emissão de poluentes.
                 {/* São apresentadas oportunidades para a adoção de práticas sustentáveis tanto por parte das empresas quanto pela administração pública, destacando a importância de realizar compras públicas de forma ambientalmente responsável. */}
               </p>
             </div>
@@ -57,12 +65,13 @@ const About = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
 
-{/* <div className="py-10 flex content-center bg-lime-600 flex-col items-center justify-center">
+{
+  /* <div className="py-10 flex content-center bg-lime-600 flex-col items-center justify-center">
         <h1 className="text-8xl text-white mt-20">Sobre o Projeto</h1>
 
         <p className="text-4xl text-white mt-16 mx-16">Este projeto é um E-commerce de produtos usados ou sem uso, tendo como inspiração o Objetivo de Desenvolvimento Sustentável (ODS) número 12 da ONU- Consumo e Produção Sustentáveis.</p>
@@ -72,8 +81,10 @@ export default About
         <div className="grid justify-items-start text-2xl text-white">
           
         </div>
-      </div> */}
-{/* <div className="flex flex-col items-center md:flex-row">
+      </div> */
+}
+{
+  /* <div className="flex flex-col items-center md:flex-row">
             <a
               href="/"
               className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
@@ -87,4 +98,5 @@ export default About
             >
               Learn more
             </a>
-          </div> */}
+          </div> */
+}

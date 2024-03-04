05a81bd49cada9fe1f6093bb8cf189721d8fc9b6
marina-mmap <marinapereira.mmap@gmail.com>
2024-03-04 12:35:21 -0300

    About - Imagens

--- ReVende-React/src/pages/about/About.tsx
+++ ReVende-React/src/pages/about/About.tsx
@@ -1,7 +1,63 @@
+import encomendaFoto from '../../assets/encomenda.jpg';
+
 const About = () => {
   return (
     <>
-      <div className="py-10 flex content-center bg-lime-600 flex-col items-center justify-center">
+    <div className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
+      <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+        <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+          <div className="max-w-xl mb-6">
+            <div>
+              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                New Colaboration
+              </p>
+            </div>
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{' '}
+              <span className="inline-block text-deep-purple-accent-400">
+                a lazy dog
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
+            </p>
+          </div>
+          <div className="flex flex-col items-center md:flex-row">
+            <a
+              href="/"
+              className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+            >
+              Apply Now
+            </a>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+          src={encomendaFoto}
+          alt=""
+        />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default About
+
+  {/* <div className="py-10 flex content-center bg-lime-600 flex-col items-center justify-center">
         <h1 className="text-8xl text-white mt-20">Sobre o Projeto</h1>
 
         <p className="text-4xl text-white mt-16 mx-16">Este projeto é um E-commerce de produtos usados ou sem uso, tendo como inspiração o Objetivo de Desenvolvimento Sustentável (ODS) número 12 da ONU- Consumo e Produção Sustentáveis.</p>
@@ -11,9 +67,4 @@ const About = () => {
         <div className="grid justify-items-start text-2xl text-white">
           
         </div>
-      </div>
-    </>
-  )
-}
-
-export default About
+      </div> */}

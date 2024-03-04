ac2666effae36be22d67a3020f23349b332f6292
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-03-04 17:06:31 -0300

    Corrigido bug na quantidade de itens no carrinho

--- ReVende-React/src/components/carrinho/Carrinho.tsx
+++ ReVende-React/src/components/carrinho/Carrinho.tsx
@@ -40,42 +40,44 @@ function Carrinho() {
     buscarProdutoPorId(produto.id);
 
     ctx.updateValorTotal();
-    
-    return (
-      <li key={produto.id} className="flex py-6">
-        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-          <img
-            src={produtoAtual?.foto}
-            alt='Imagem do Produto'
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
 
-        <div className="ml-4 flex flex-1 flex-col">
-          <div>
-            <div className="flex justify-between text-base font-medium text-gray-900">
-              <h3>
-                <Link to={`/editarProduto/${produto.id}`}>{produtoAtual?.nome}</Link>
-              </h3>
-              <p className="ml-4">{produtoAtual?.preco}</p>
-            </div>
-            <p className="mt-1 text-sm text-gray-500">Vendedor: {produtoAtual?.usuario?.nome}</p>
+    if(produtoAtual?.quantidade != 0) {
+      return (
+        <li key={produto.id} className="flex py-6">
+          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+            <img
+              src={produtoAtual?.foto}
+              alt='Imagem do Produto'
+              className="h-full w-full object-cover object-center"
+            />
           </div>
-          <div className="flex flex-1 items-end justify-between text-sm">
-            <p className="text-gray-500">Qtd {produtoAtual?.quantidade}</p>
-
-            <div className="flex">
-              <button
-                type="button" onClick={() => {ctx.removerProduto(produto.id)}}
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Remover
-              </button>
+  
+          <div className="ml-4 flex flex-1 flex-col">
+            <div>
+              <div className="flex justify-between text-base font-medium text-gray-900">
+                <h3>
+                  <Link to={`/editarProduto/${produto.id}`}>{produtoAtual?.nome}</Link>
+                </h3>
+                <p className="ml-4">{produtoAtual?.preco}</p>
+              </div>
+              <p className="mt-1 text-sm text-gray-500">Vendedor: {produtoAtual?.usuario?.nome}</p>
+            </div>
+            <div className="flex flex-1 items-end justify-between text-sm">
+              <p className="text-gray-500">Qtd {produto.quantidade}</p>
+  
+              <div className="flex">
+                <button
+                  type="button" onClick={() => {ctx.removerProduto(produto.id)}}
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Remover
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </li>
-    );
+        </li>
+      );
+    }
   }
 
   return (

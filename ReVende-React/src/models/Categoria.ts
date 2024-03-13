ec9e0055e176f3eded330338e5dfd991f89bcd75
E-commerce Grupo 03 <156813753+ecommerceGrupo03@users.noreply.github.com>
2024-03-13 14:52:53 -0300

    Delete ReVende-React directory

--- ReVende-React/src/models/Categoria.ts
+++
@@ -1,5 +0,0 @@
-export default interface Categoria {
-    id: number;
-    nome: string;
-    descricao: string;
-  }
--- ReVende-React/src/models/ProdutoCarrinho.ts
+++
@@ -1,4 +0,0 @@
-export default interface ProdutoCarrinho{
-    id: number;
-    quantidade: number; 
-}

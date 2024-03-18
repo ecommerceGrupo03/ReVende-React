33f0eaf1a971897a9e34002ec02ca6bce20bb5df
mariliatostesdev <mahtostes.dev@gmail.com>
2024-03-18 19:12:49 -0300

    responsive InfoProduto

--- src/components/produto/modalProduto/InfoProduto.tsx
+++ src/components/produto/modalProduto/InfoProduto.tsx
@@ -27,7 +27,7 @@ function InfoProduto({produto, fecharInfo}: InfoProdutoProps) {
   return (
     <>
       <div className="min-w-screen min-h-screen bg-white-300 flex items-center overflow-hidden relative">
-        <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+        <div className="xsm:w-5/6 md:w-[90%] max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
           <XCircle size={30} onClick={fecharInfo} color="#5A6973" className="absolute top-0 right-0 m-4 hover:cursor-pointer"/>
           <div className="md:flex items-center -mx-10">
             <div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
@@ -38,21 +38,21 @@ function InfoProduto({produto, fecharInfo}: InfoProdutoProps) {
                   alt="fotoProduto"
                 />
 
-                <div className="border-4 border-yellow-200 absolute top-10 bottom-10 left-10 right-10 z-0"></div>
+                <div className="border-4 border-gray-200 absolute top-10 bottom-10 left-10 right-10 z-0"></div>
               </div>
             </div>
             <div className="w-full md:w-1/2 px-10">
-              <div className="mb-10">
-                <h1 className="font-bold uppercase text-3xl mb-5">
+              <div className="mb-5">
+                <h1 className="font-bold uppercase text-3xl mb-5 xsm:text-xl md:text-2xl">
                   {produto.nome}
                 </h1>
-                <p className="text-md">{produto.descricao}</p>
-                <div className="mt-8">
-                  <p className="text-lg">Vendedor: {produto.usuario.nome}</p>
+                <p className="text-lg xsm:text-sm md:text-base">{produto.descricao}</p>
+                <div className="mt-4">
+                  <p className="text-xl xsm:text-base md:text-lg">Vendedor: {produto.usuario.nome}</p>
                 </div>
               </div>
               <div>
-                <div className="inline-block align-bottom mr-5">
+                <div className="inline-block align-bottom mr-5 my-5">
                   <span className="text-2xl leading-none align-baseline">
                     R$
                   </span>
